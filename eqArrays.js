543ec31938c50c5ea49039bd31f45856b907135e
carolineolagunju <olamidejr21@gmail.com>
2023-05-05 19:32:26 -0600

    function-countOnly

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,9 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅ Assertion passed: ${actual} === ${expected}`);
-  } else {
+  if (!actual === expected) {
     console.log(`🛑🛑 Assertion failed: ${actual} !== ${expected}`);
-  }
-}
+  } 
+  console.log(`✅✅ Assertion passed: ${actual} === ${expected}`);
+};
 
 
 const eqArrays = function(arr1, arr2) {
