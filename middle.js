98d973e9aecc8f49c70202c066a81457be779e71
carolineolagunju <olamidejr21@gmail.com>
2023-05-19 18:46:58 -0600

    refactored code

--- middle.js
+++ middle.js
@@ -1,4 +1,7 @@
 const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
@@ -7,34 +10,37 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
+
+
+
 const assertArraysEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-    console.log("Passed comparison.");
+    console.log("😊😊😊 Passed comparison.");
   } else {
-    console.log("Failed comparison.");
+    console.log("😡😡😡 Failed comparison.");
   }
 };
+
 
 
 
 const middle = function(arr) {
+  let result = [];
   if (arr.length < 3) {
-    return [];
+    return result;
   }
 
   if (arr.length % 2 === 0) {
-    let result = [];
-    const num = arr.length / 2;
-    result.push(arr[num - 1]);
-    result.push(arr[num]);
+    const halfLength = arr.length / 2;
+    result.push(arr[halfLength - 1])
+    result.push(arr[halfLength]);
     return result;
   }
 
   if (arr.length % 2 !== 0) {
-    let result = [];
-    const num = arr.length / 2;
-    const roundedNum = Math.ceil(num);
-    result.push(arr[roundedNum - 1]);
+    const halfLength = arr.length / 2;
+    const roundedLength = Math.floor(halfLength);
+    result.push(arr[roundedLength]);
     return result;
   }
 };
@@ -42,6 +48,7 @@ const middle = function(arr) {
 
 
 
+assertArraysEqual(middle([1,2]), []); //pass
 assertArraysEqual(middle([1,2,3]), [2]);
-assertArraysEqual(middle([1,2,3,4]), [2,3]);
-assertArraysEqual(middle([1,2,3,4,5]), [3]);
+assertArraysEqual(middle([1,2,3,4]), [2,3]); //pass
+assertArraysEqual(middle([1,2,3,4,5]), [3,4]); // fail
