1f83d836d767794ee9bfbd3775c4bb3fcf691e10
carolineolagunju <olamidejr21@gmail.com>
2023-05-21 19:29:02 -0600

    added test data

--- countLetters.js
+++ countLetters.js
@@ -1,30 +1,31 @@
 const assertEqual = function(actual, expected) {
-  if (!actual === expected) {
+  if (actual === expected) {
+    console.log(`✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
     console.log(`🛑🛑 Assertion failed: ${actual} !== ${expected}`);
   }
-
-  console.log(`✅✅ Assertion passed: ${actual} === ${expected}`);
 };
 
 
 
-const countLetters = function(string) {
+const countLetters = letters => {
   const result = {};
-
-  for (const letter of string) {
-    if (letter === " ") {
+  for (const letter of letters) {
+    if (letter === (" ")) {
       continue;
     }
-
-    if (!result[letter]) {
-      result[letter] = 0;
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
     }
-
-    result[letter]++;
-
   }
-
   return result;
 };
 
-assertEqual(countLetters('lighthouse'));
+
+
+const letters = countLetters("I am a student of lighthouse labs");
+assertEqual(letters["s"], 3);  //pass
+assertEqual(letters["o"], 3);  //fail
+
