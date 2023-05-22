791e02ddeb8d0522746382084844af9ed9925ce8
carolineolagunju <olamidejr21@gmail.com>
2023-05-21 18:37:43 -0600

    lint code

--- countOnly.js
+++ countOnly.js
@@ -1,17 +1,16 @@
 const assertEqual = function(actual, expected) {
-  if (!actual === expected) {
+  if (actual === expected) {
+    console.log(`✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
     console.log(`🛑🛑 Assertion failed: ${actual} !== ${expected}`);
   }
-
-  console.log(`✅✅ Assertion passed: ${actual} === ${expected}`);
 };
 
 
 
 
-const countOnly = function(allItems, itemsToCount) {
+const countOnly = (allItems, itemsToCount) => {
   const results = {};
-
   for (const item of allItems) {
     if (itemsToCount[item]) {
       if (results[item]) {
@@ -21,35 +20,13 @@ const countOnly = function(allItems, itemsToCount) {
       }
     }
   }
+
   return results;
 };
 
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
-
-
-
-
-
-//Another possible way
-/*const countOnly = function(allItems, itemsToCount) {
+/*//Another possible way
+const countOnly = function(allItems, itemsToCount) {
   const results = {}
 
   for (const item of allItems) {
@@ -70,4 +47,22 @@ assertEqual(result1["Agouhanna"], undefined);
 
 
 
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
 
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
