125892a484f362cc96897e8213809096e92f036a
carolineolagunju <olamidejr21@gmail.com>
2023-05-22 19:36:34 -0600

    added code to check for keys with array value

--- eqObjects.js
+++ eqObjects.js
@@ -1,3 +1,18 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅ Assertion passed: ${actual} === ${expected}`);
@@ -8,27 +23,35 @@ const assertEqual = function(actual, expected) {
 
 
 
+
 const eqObjects = (object1, object2) => {
 
   let object1Key = Object.keys(object1);
 
   let object2Key = Object.keys(object2);
+
+  let returnedValue = false;
+
   //since keys will be printed as an array, it makes sense to call .length
   if (object1Key.length !== object2Key.length) {
     return false;
   }
 
   for (const key of object1Key) {
-    if (object1[key] !== object2[key]) {
-      return false;
+    //checking if key value is an array
+    if (object1[key].isArray === object2[key].isArray) {
+      returnedValue = eqArrays(object1[key], object2[key]);
+    } else if (object1[key] === object2[key]) {
+      returnedValue = true;
     }
   }
 
-  return true;
+  return returnedValue;
 };
 
-const car = {brand: "Ford", model: "Escape", year: 2023, wheel: "All wheel drive"};
 
+//Test Data
+const car = {brand: "Ford", model: "Escape", year: 2023, wheel: "All wheel drive"};
 
 const carolineCar = {brand: "Ford", model: "Escape", year: 2023, wheel: "All wheel drive"};
 
@@ -36,7 +59,13 @@ const olagunju = {firstName: "Caroline", lastName: "Olagunju", age: 31, job: "We
 
 const caroline = {firstName: "Caroline", lastName: "Olagunju", age: 31, job: "Web developer", school: "LHL"};
 
+const shirt = {colours: ["white", "green"], size: 2};
+
+const myShirt = {colours: ["white", "green"], size: 2};
+
+
 
 assertEqual(eqObjects(car, carolineCar), true);//pass
 assertEqual(eqObjects(olagunju, caroline), true);//fail because the length is not equal
+assertEqual(eqObjects(shirt, myShirt), true);
 
