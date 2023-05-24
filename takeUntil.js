aa55f7bba0ce4dda1810a19d024266e7051f0f0f
carolineolagunju <olamidejr21@gmail.com>
2023-05-24 11:17:57 -0600

    implemented function takeUntil

---
+++ takeUntil.js
@@ -0,0 +1,45 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log("😊😊😊 Passed comparison.");
+  } else {
+    console.log("😡😡😡 Failed comparison.");
+  }
+};
+
+
+
+
+const takeUntil = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    if (callback(item)) {
+      return results;
+    }
+    results.push(item);
+  }
+  return results;
+};
+
+
+const peoples = ["carol", "success", "james", "esther"];
+const result1 = takeUntil(peoples, people => people === "esther");
+
+const data1 = [1, 2, 5, 7, 2, -1, 8, 4, 5];
+const result2 = takeUntil(data1, data => data > 7);
+
+assertArraysEqual(result1, ["carol", "success", "james"]);//pass
+assertArraysEqual([1, 2, 5, 7, 2, 0], result2);//fail because the last element in the array is not the same value
