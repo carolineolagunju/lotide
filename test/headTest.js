1ef1c6f26544bdcd7f452d7785f9ebe7ace95e22
carolineolagunju <olamidejr21@gmail.com>
2023-06-07 13:23:17 -0600

    refactored code

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,14 @@
 const assertEqual = require('../assertEqual')
 const head = require('../head');
 
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");//pass
-assertEqual(head([5,6,7]), 5);//pass
-assertEqual(head([10]), 5);//fail
-assertEqual(head([]), 0);//fail
+//pass
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+//pass
+assertEqual(head([5,6,7]), 5);
+
+//fail
+assertEqual(head([10]), 5);
+
+//fail
+assertEqual(head([]), 0);
