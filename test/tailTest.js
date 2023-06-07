228ea168dc06e87a055e5116785109ae4339cafd
carolineolagunju <olamidejr21@gmail.com>
2023-06-07 13:27:43 -0600

    clean code

--- test/tailTest.js
+++ test/tailTest.js
@@ -8,9 +8,17 @@ const someNumbers = tail(["2", "10", "5"]);
 const places = tail(["Canada", "USA", "Mexico", "Nigeria" ]);
 
 
+ //should pass because the length of emptyArr is 0
+assertEqual(emptyArr.length, 0);
 
-assertEqual(emptyArr.length, 0); // should pass because the length of emptyArr is 0
-assertEqual(words.length, 1); // fail, length of words is now 0
-assertEqual(names.length, 1); // pass, because the length of names is now 1
+ //fail, length of words is now 0
+assertEqual(words.length, 1);
+
+ //pass, because the length of names is now 1
+assertEqual(names.length, 1);
+
+//should pass because length is 2
 assertEqual(someNumbers.length, 2);
-assertEqual(places.length, "Light"); // should fail because number is !== string
+
+ //should fail because number is !== string
+assertEqual(places.length, "Light");
