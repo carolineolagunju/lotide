00f7f2fd0c09d328244458609de993c22ca3d056
carolineolagunju <olamidejr21@gmail.com>
2023-06-07 16:38:12 -0600

    test code with mocha & chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,14 +1,12 @@
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
 const head = require('../head');
 
-//pass
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 1 for [1,2,3]", () => {
+    assert.strictEqual(head([1,2,3]), 1);
+  });
 
-//pass
-assertEqual(head([5,6,7]), 5);
-
-//fail
-assertEqual(head([10]), 5);
-
-//fail
-assertEqual(head([]), 0);
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5')
+  });
+});
